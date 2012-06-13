26e569416b3d2582b8f06af5d33cfe7977cdbfd0
Carol Hansen <carolbhansen@gmail.com>
2012-06-13 11:49:12 -0500

    updates

--- neo4j/src/main/java/org/neo4j/neo4j/OSMImporterNew.java
+++ neo4j/src/main/java/org/neo4j/neo4j/OSMImporterNew.java
@@ -1,12 +1,19 @@
 package org.neo4j.neo4j;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
+//import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.index.Index;
-import org.neo4j.neo4j.OSMway;
+//import org.neo4j.graphdb.index.Index;
+//import org.neo4j.neo4j.OSMway;
 
 public class OSMImporterNew 
 {
@@ -14,8 +21,8 @@ public class OSMImporterNew
 	private static final String osmImport_DB = "target/osmImport-db";
 	private static GraphDatabaseService graphDb;
 	private static String osmXmlFilePath = "C:\\Users\\Carol\\Desktop\\GSoC\\osm\\liechtenstein.osm";
-    private static Index<Node> nodeIndex;//will I need to create my own index? Or is this done automatically?
-	
+    //private static Index<Node> nodeIndex;//will I need to create my own index? Or is this done automatically?
+    
 	
 	
 	//Elements and attributes to be read from the XML file
@@ -36,16 +43,62 @@ public class OSMImporterNew
 	        REFERENCE_NODE
 	}
 	
-	public static void main( final String[] args )
+	public static void main( final String[] args ) throws FileNotFoundException
     {
         // START SNIPPET: startDb
         graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( osmImport_DB );
-        nodeIndex = graphDb.index().forNodes( "nodes" ); //what is stored in this variable?
+        //nodeIndex = graphDb.index().forNodes( "nodes" ); //what is stored in this variable?
         registerShutdownHook();
+        XMLInputFactory factory = XMLInputFactory.newInstance();
         // END SNIPPET: startDb
 
         // START SNIPPET: addUsers
         Transaction tx = graphDb.beginTx();
+      		
+      		try 
+      		{
+
+      			XMLStreamReader streamReader = factory.createXMLStreamReader(
+      				    new FileReader(osmXmlFilePath));
+
+      			while(streamReader.hasNext())
+      			{
+      				streamReader.next();
+      				
+      				if(streamReader.getEventType() == XMLStreamReader.START_ELEMENT)
+      				{
+      					if(streamReader.getLocalName() == "way")
+      					{
+      						int count = streamReader.getAttributeCount();
+      						for(int i = 0; i < count; i++)
+      						{
+      							System.out.println("Attribute Value: " + streamReader.getAttributeValue(i));
+      							System.out.println("Attribute Name: " + streamReader.getAttributeName(i));
+      			                System.out.println("Attribute Namespace: " + streamReader.getAttributeNamespace(i));
+      			                System.out.println("Attribute Type: " + streamReader.getAttributeType(i));
+      			                System.out.println("Attribute Prefix: " + streamReader.getAttributePrefix(i));
+      						}
+      					}//end if(getLocalName == "tag")
+      				}//end if(getEventType)
+      			}//end while
+      		    
+      		  tx.success();
+      		} 
+      		
+      		catch (XMLStreamException e) 
+      		{
+      		    e.printStackTrace();
+      		}
+      		
+      		 finally
+             {
+                 tx.finish();
+             }
+             System.out.println( "Shutting down database ..." );
+             shutdown();
+      		
+      		
+        /*
         try
         {
             // Create users sub reference node
@@ -63,9 +116,11 @@ public class OSMImporterNew
         {
         	
         }
-        
+        */
     }//end main
 	
+	
+	/*
 	//what data or properties should be within 
 	private static Node createAndIndexWayNode( final String ??? )
 	{
@@ -74,6 +129,8 @@ public class OSMImporterNew
 	    nodeIndex.add( node, USERNAME_KEY, username );
 	    return node;
 	}
+	*/
+	
 	
 	private static void registerShutdownHook()
 	{
@@ -88,7 +145,12 @@ public class OSMImporterNew
 	              shutdown();
 	          }
 	       } );
-	 }//end registerShutdownHook()
+	}//end registerShutdownHook()
+	
+    private static void shutdown()
+    {
+        graphDb.shutdown();
+    }
 	
     /*//The following is for using the StAX API
     try 
